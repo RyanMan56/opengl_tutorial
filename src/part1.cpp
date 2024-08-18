64e638c1934cc77e21b04cc388cbbad9e77e7ac6
Ryan <ryanman56@gmail.com>
2024-08-18 21:00:22 +0100

    multiply texture colours by our colours

--- src/part1.cpp
+++ src/part1.cpp
@@ -29,7 +29,7 @@ void part1(GLFWwindow *window)
         // Positions        // Colors       // Texture coords
         // 0.5f, -0.5f, 0.0f,  // bottom right
         -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom left
-        -0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f   // top left
+        -0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f   // top left
     };
     unsigned int indices[]{
         0, 1, 3, // First triangle
