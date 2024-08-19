8bb2519f62c8baa69c4d4e89cbe66936d8b6ad70
Ryan <ryanman56@gmail.com>
2024-08-19 23:13:49 +0100

    exercise 1: horizontally flip the face

--- src/shaders/basic.fs.glsl
+++ src/shaders/basic.fs.glsl
@@ -13,5 +13,5 @@ void main()
 {
   // mix(...) linearly interpolates between 2 colours. If the third argument is 0.0 the first input will be returned, if it's 1.0 then the second input will be returned.
   // A value of 0.2 will return 80% of the first input colour and 20% of the second input colour
-  FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2) * vec4(ourColor, 1.0);
+  FragColor = mix(texture(texture1, TexCoord), texture(texture2, vec2(1.0 - TexCoord.x, TexCoord.y)), 0.2) * vec4(ourColor, 1.0);
 }
